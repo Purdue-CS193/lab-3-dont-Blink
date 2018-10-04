07775d1a0c3f49380ded20550ef857410c46cde4
dont-Blink <m.rosen@rosenrealm.com>
2018-10-04 09:14:38 -0400

    debugged

--- src/StringUtils.java
+++ src/StringUtils.java
@@ -18,6 +18,7 @@ public class StringUtils {
                     --> res = "SFlanner"
          */
         if(res.length() > 8) {
+
             res = res.substring(0, 8);
         }
         /* Convert the name to lower-case
